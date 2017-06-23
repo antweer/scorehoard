7f0329199c68de9cb89d4eba966bbf026bf0f952
antweer <trajwani@gmail.com>
2017-06-23 12:04:26 -0500

    switched out sensitive info with env fields

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var promise = require('bluebird');
 var pgp = require('pg-promise')({
   promiseLib: promise
 });
-var db = pgp({database: 'scorehoard', user:'postgres'});
+var db = pgp({database: process.env['DB_NAME'], user: process.env['DB_USER']});
 var apikey = require("apikeygen").apikey;
 
 // Crypto configuration
@@ -512,6 +512,6 @@ app.get('/verify/:key', function(request, response, next){
 })
 
 //Listener -- Change for production
-app.listen(9010, function(){
+app.listen(process.env['PORT'], function(){
   console.log('I am now listening... I am now sentient... Hello')
 });
