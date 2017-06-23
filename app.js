62c572adfb40f18c1f0eae47a97e86ad7809dc29
Jordan Benner <jordanbenner@outlook.com>
2017-06-23 12:04:50 -0500

    merge master

--- app.js
+++ app.js
@@ -512,11 +512,7 @@ app.get('/verify/:key', function(request, response, next){
 })
 
 //Listener -- Change for production
-<<<<<<< HEAD
-app.listen(8001, function(){
-  console.log('Listening on port 8000')
-=======
 app.listen(9010, function(){
   console.log('I am now listening... I am now sentient... Hello')
->>>>>>> master
+
 });
